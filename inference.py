4974b3b3086f81e4745cb80d2e55452cefed1c20
abhishek-appypie <89147812+abhishek-appypie@users.noreply.github.com>
2021-08-26 17:06:10 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -45,6 +45,13 @@ def lambda_handler(event, context):
     # inference images
 
     imi = Image.open(requests.get(url, stream=True).raw)
+    basewidth=1500
+    baseheight=1500
+    if imi.size[0]>1000 and imi.size[1]>1500:
+        wpercent = (basewidth/float(imi.size[0]))
+        hsize = int((float(imi.size[1])*float(wpercent)))
+        imi = imi.resize((basewidth,hsize), image.ANTIALIAS)
+        
 
     # unify image channels to 3
     imi = np.asarray(imi)
@@ -87,27 +94,39 @@ def lambda_handler(event, context):
     matte = F.interpolate(matte, size=(im_h, im_w), mode='area')
     matte = matte[0][0].data.cpu().numpy()
     im_name='1'
-    matte_name = im_name.split('.')[0] + '.png'
-    # print(matte.shape)
-    Image.fromarray(((matte * 255).astype('uint8')), mode='L').save(os.path.join(outputp, matte_name))
+    
+#     matte_name = im_name.split('.')[0] + '.png'
+#     # print(matte.shape)
+#     Image.fromarray(((matte * 255).astype('uint8')), mode='L').save(os.path.join(outputp, matte_name))
 
-    matte = Image.open(os.path.join(outputp, matte_name))
-    # print(matte.shape)
+#     matte = Image.open(os.path.join(outputp, matte_name))
+#     # print(matte.shape)
+#     matte = np.repeat(np.asarray(matte)[:, :, None], 3, axis=2) / 255
+#     w, h = im_w, im_h
+#     rw, rh = 800, int(h * 800 / (3 * w))
+#     foreground = imi * matte + np.full(imi.shape, 255) * (1 - matte)
+#     cv2.imwrite(os.path.join(outputp, matte_name),foreground)
+#     Image.fromarray((foreground.astype('uint8'))).save(os.path.join(outputp, '2.png'))
+#     try:
+#       s3 = boto3.client("s3")
+#       s3.put_object(Bucket='abhishekimagebucket', Key="12", Body=open("/tmp/test2.mp4", "rb").read())
+#     except:
+#       pass
+    outputp='/tmp/'
+    matte=matte*255
     matte = np.repeat(np.asarray(matte)[:, :, None], 3, axis=2) / 255
-    w, h = im_w, im_h
-    rw, rh = 800, int(h * 800 / (3 * w))
-    foreground = imi * matte + np.full(imi.shape, 255) * (1 - matte)
-    cv2.imwrite(os.path.join(outputp, matte_name),foreground)
     Image.fromarray((foreground.astype('uint8'))).save(os.path.join(outputp, '2.png'))
     try:
-      s3 = boto3.client("s3")
-      s3.put_object(Bucket='abhishekimagebucket', Key="12", Body=open("/tmp/test2.mp4", "rb").read())
+        s3 = boto3.client('s3',
+                          aws_access_key_id: 'AKIA3TO5KM7D3HRVTRRV',
+                          aws_secret_access_key: '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',
+                          region_name: 'us-east-1')
+        s3.put_object(Bucket='abhishekimagebucket', Key="123.png", Body=open(os.path.join(outputp,'2.png'), "rb").read())
     except:
-      pass
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Its working great!')
-    }
+        pass
+    
+    
+    return 'https://abhishekimagebucket.s3.amazonaws.com/123.png'
 
     
 # print(lambda_handler("https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",2))
